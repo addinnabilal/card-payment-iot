6ca9099a6e9a41a5dc49d92d8bc774b98acb37df
addinnabilal <13520045@std.stei.itb.ac.id>
2024-03-30 00:09:45 +0700

    rollback before adding firebase

--- api/index.py
+++ api/index.py
@@ -1,22 +1,22 @@
 from fastapi import FastAPI, HTTPException
-from pyrebase import pyrebase
-import os
+# from pyrebase import pyrebase
+# import os
 
-# Firebase configuration from environment variables
-firebaseConfig = {
-  'apiKey': os.environ.get('API_KEY'),
-  'authDomain': os.environ.get('AUTH_DOMAIN'),
-  'databaseURL': os.environ.get('DATABASE_URL'),
-  'projectId': os.environ.get('PROJECT_ID'),
-  'storageBucket': os.environ.get('STORAGE_BUCKET'),
-  'messagingSenderId': os.environ.get('MESSAGING_SENDER_ID'),
-  'appId': os.environ.get('APP_ID'),
-  'measurementId': os.environ.get('MEASUREMENT_ID')
-}
+# # Firebase configuration from environment variables
+# firebaseConfig = {
+#   'apiKey': os.environ.get('API_KEY'),
+#   'authDomain': os.environ.get('AUTH_DOMAIN'),
+#   'databaseURL': os.environ.get('DATABASE_URL'),
+#   'projectId': os.environ.get('PROJECT_ID'),
+#   'storageBucket': os.environ.get('STORAGE_BUCKET'),
+#   'messagingSenderId': os.environ.get('MESSAGING_SENDER_ID'),
+#   'appId': os.environ.get('APP_ID'),
+#   'measurementId': os.environ.get('MEASUREMENT_ID')
+# }
 
-firebase = pyrebase.initialize_app(firebaseConfig)
-auth = firebase.auth()
-db = firebase.database()
+# firebase = pyrebase.initialize_app(firebaseConfig)
+# auth = firebase.auth()
+# db = firebase.database()
 
 app = FastAPI()
 
@@ -33,7 +33,7 @@ def pay():
     
     try:
         # Push data to 'users' node in the Firebase database
-        results = db.child("users").push(data)
+        # results = db.child("users").push(data)
         # Return the results along with a success message
         return {"success": True, "data": results}
     except Exception as e:
